544153ce43b2b2a9df7e8c6c4e2aa00d64715d5e
mehdi harzallah <96887412+opestro@users.noreply.github.com>
2024-11-09 15:14:46 +0100

    Initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,74 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  app: {
+    pageTransition: { name: "page", mode: "out-in" },
+  },
+  css: ["~/assets/css/main.css"],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  typescript: {
+    shim: false,
+    strict: true,
+  },
+  runtimeConfig: {
+    apiSecret: "",
+    public: {
+      apiBase: "/api",
+    },
+  },
+  modules: [
+    "@pinia/nuxt",
+    "@pinia-plugin-persistedstate/nuxt",
+    "@vite-pwa/nuxt",
+    "@vueuse/nuxt",
+  ],
+  pwa: {
+    registerType: "autoUpdate",
+    manifest: {
+      name: "Nuxt 3 admin template",
+      short_name: "Nuxt3 Admin",
+      theme_color: "#ffffff",
+      icons: [
+        {
+          src: "pwa-192x192.png",
+          sizes: "192x192",
+          type: "image/png",
+        },
+        {
+          src: "pwa-512x512.png",
+          sizes: "512x512",
+          type: "image/png",
+        },
+        {
+          src: "pwa-512x512.png",
+          sizes: "512x512",
+          type: "image/png",
+          purpose: "any maskable",
+        },
+      ],
+    },
+    workbox: {
+      globPatterns: ["**/*.{js,css,html,png,svg,ico}"],
+    },
+    injectManifest: {
+      globPatterns: ["**/*.{js,css,html,png,svg,ico}"],
+    },
+    client: {
+      installPrompt: true,
+      // you don't need to include this: only for testing purposes
+      // if enabling periodic sync for update use 1 hour or so (periodicSyncForUpdates: 3600)
+      periodicSyncForUpdates: 20,
+    },
+    devOptions: {
+      enabled: true,
+      suppressWarnings: true,
+      navigateFallback: "/",
+      navigateFallbackAllowlist: [/^\/$/],
+      type: "module",
+    },
+  },
+});
